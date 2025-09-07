d690f2b99aacbfae6e5831b351b5fa53d0681c53
MarkHash <makoto.hashimoto06@gmail.com>
2025-09-08 09:58:47 +1000

    migrated to postgreSQL

--- bacnet_project/settings.py
+++ bacnet_project/settings.py
@@ -89,13 +89,23 @@ WSGI_APPLICATION = "bacnet_project.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.sqlite3",
+#         "NAME": BASE_DIR / "db.sqlite3",
+#     }
+# }
+
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "bacnet_django",
+        "USER": "bacnet_user",
+        "PASSWORD": "password",
+        "HOST": "localhost",
+        "PORT": "5432",
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
